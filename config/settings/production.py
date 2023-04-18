244b053027c55a33d00bf355929adae3aaacb00a
Ramses Martinez <ramses.mtz96@gmail.com>
2023-04-18 11:49:22 -0600

    Update aws s3 settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -76,7 +76,8 @@ AWS_S3_REGION_NAME = env("DJANGO_AWS_S3_REGION_NAME", default=None)
 AWS_S3_CUSTOM_DOMAIN = env("DJANGO_AWS_S3_CUSTOM_DOMAIN", default=None)
 aws_s3_domain = AWS_S3_CUSTOM_DOMAIN or f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL = "private"
+AWS_DEFAULT_ACL = None
+AWS_S3_VERITY = True
 AWS_S3_SIGNATURE_VERSION = "s3v4"
 # STATIC
 # ------------------------
@@ -85,7 +86,7 @@ COLLECTFAST_STRATEGY = "collectfast.strategies.boto3.Boto3Strategy"
 STATIC_URL = f"https://{aws_s3_domain}/static/"
 # MEDIA
 # ------------------------------------------------------------------------------
-DEFAULT_FILE_STORAGE = "finmancorp_python.utils.storages.MediaRootS3Boto3Storage"
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3BotoS3Storage"
 MEDIA_URL = f"https://{aws_s3_domain}/media/"
 
 # EMAIL
